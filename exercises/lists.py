2fb2e48d3b1a7d832888635dc87d36d89a08467b
Roman Baksheev <rombaks.dev@gmail.com>
2022-10-01 20:11:57 +0500

    Fix lists.py

--- exercises/lists.py
+++ exercises/lists.py
@@ -2,20 +2,76 @@ class ListExercise:
     @staticmethod
     def replace(input_list: list[int]) -> list[int]:
         """
-        Заменить все положительные элементы целочисленного списка на максимальное значение
-        элементов списка.
-        :param input_list: Исходный список
-        :return: Список с замененными элементами
+        Replaces positive elements in the list by max element.
+        :param input_list: Input list
+        :return: Replaced list
         """
-        pass
+        if len(input_list) < 2:
+            return input_list
+
+        max_element = ListExercise.__get_max(input_list)
+        replaced_list = input_list[:]
+
+        for index, element in enumerate(input_list):
+            if element > 0:
+                replaced_list[index] = max_element
+
+        return replaced_list
+
+    @staticmethod
+    def __get_max(input_list: list[int]) -> int:
+        """
+        Returns the item with the highest value in the list.
+        :param input_list: Input list
+        :return: Max element
+        """
+        max_element = input_list[0]
+
+        for element in input_list:
+            if element > max_element:
+                max_element = element
+
+        return max_element
 
     @staticmethod
     def search(input_list: list[int], query: int) -> int:
         """
-        Реализовать двоичный поиск
-        Функция должна возвращать индекс элемента
-        :param input_list: Исходный список
-        :param query: Искомый элемент
-        :return: Номер элемента
+        Returns the index of searched element using binary search.
+        If element not in a list returns -1.
+        :param input_list: Input list
+        :param query: Searched element
+        :return: Index of searched element
         """
-        pass
+        sorted_list = sorted(input_list)
+        low = 0
+        high = len(sorted_list) - 1
+
+        searched_index = ListExercise.__binary_search(sorted_list, query, low, high)
+
+        return searched_index
+
+    @staticmethod
+    def __binary_search(sorted_list: list[int], query: int, low: int, high: int) -> int:
+        """
+        Returns the index of searched element using recursive binary search.
+        If element not in a list returns -1.
+        :param sorted_list: Sorted list
+        :param query: Searched element
+        :low: Lowest pointer
+        :high: Highest pointer
+        :return: Index of searched element
+        """
+        if high >= low:
+
+            mid = (high + low) // 2
+            if sorted_list[mid] == query:
+                return mid
+
+            elif sorted_list[mid] > query:
+                return ListExercise.__binary_search(sorted_list, query, low, mid - 1)
+
+            else:
+                return ListExercise.__binary_search(sorted_list, query, mid + 1, high)
+
+        else:
+            return -1
