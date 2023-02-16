c21a039a637b3d92dcc06d1b7b86151265eaf56c
VolodimirV <vladimir_veko@mail.ru>
2023-02-16 17:30:42 +0300

    task from lists ready

--- lists/lists.py
+++ lists/lists.py
@@ -8,7 +8,17 @@ class ListExercise:
         :param input_list: Исходный список
         :return: Список с замененными элементами
         """
-        pass
+        max_number = input_list[0] if len(input_list) > 0 else None
+        for item in input_list:
+            if item > max_number:
+                max_number = item
+        output_list = []
+        for item in input_list:
+            if item > 0:
+                output_list.append(max_number)
+            else:
+                output_list.append(item)
+        return output_list
 
     @staticmethod
     def search(input_list: list[int], query: int) -> int:
@@ -20,4 +30,16 @@ class ListExercise:
         :param query: Искомый элемент
         :return: Номер элемента
         """
-        pass
+        low = 0
+        high = len(input_list) - 1
+
+        while low <= high:
+            mid = (low + high) // 2
+            if input_list[mid] == query:
+                return input_list.index(input_list[mid])
+            else:
+                if query < input_list[mid]:
+                    high = mid - 1
+                else:
+                    low = mid + 1
+        return -1
