959e5c22164a1fbb8a8cd6f816d1446151de4756
https://github.com/AndreyPanda <andrey.pankin.nch@gmail.com>
2023-03-10 09:53:05 +0300

    Task "Lists" is completed.

--- lists/lists.py
+++ lists/lists.py
@@ -8,7 +8,9 @@ class ListExercise:
         :param input_list: Исходный список
         :return: Список с замененными элементами
         """
-        pass
+        sorted_list = input_list.copy()
+        sorted_list.sort()
+        return [sorted_list[-1] if elem > 0 else elem for elem in input_list]
 
     @staticmethod
     def search(input_list: list[int], query: int) -> int:
@@ -20,4 +22,20 @@ class ListExercise:
         :param query: Искомый элемент
         :return: Номер элемента
         """
-        pass
+        if len(input_list) == 0:
+            return -1
+
+        mid = len(input_list) // 2
+
+        if input_list[mid] == query:
+            return mid
+
+        elif input_list[mid] > query:
+            return ListExercise.search(input_list[:mid], query)
+
+        else:
+            x = ListExercise.search(input_list[mid + 1 :], query)
+            if x == -1:
+                return -1
+            else:
+                return x + mid + 1
