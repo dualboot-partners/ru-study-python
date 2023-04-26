39ffc3e6d648c5320c10df2017722b2da8196ecc
Chera <chera1904@mail.ru>
2023-04-26 14:00:37 +0600

    Test flask

--- flask_exercises/flask_excercises.py
+++ flask_exercises/flask_excercises.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, request
 
 
 class FlaskExercise:
@@ -7,25 +7,59 @@ class FlaskExercise:
     В данной задаче все пользователи хранятся в одном словаре, где ключ - это имя пользователя,
     а значение - его параметры. {"user1": {"age": 33}, "user2": {"age": 20}}
     Словарь (dict) хранить в памяти, он должен быть пустым при старте flask.
-
     POST /user - создание пользователя.
     В теле запроса приходит JSON в формате {"name": <имя пользователя>}.
     Ответ должен вернуться так же в JSON в формате {"data": "User <имя пользователя> is created!"}
     со статусом 201.
     Если в теле запроса не было ключа "name", то в ответ возвращается JSON
     {"errors": {"name": "This field is required"}} со статусом 422
-
     GET /user/<name> - чтение пользователя
     В ответе должен вернуться JSON {"data": "My name is <name>"}. Статус 200
-
     PATCH /user/<name> - обновление пользователя
     В теле запроса приходит JSON в формате {"name": <new_name>}.
     В ответе должен вернуться JSON {"data": "My name is <new_name>"}. Статус 200
-
     DELETE /user/<name> - удаление пользователя
     В ответ должен вернуться статус 204
     """
 
     @staticmethod
     def configure_routes(app: Flask) -> None:
-        pass
+        users: dict = {}
+
+        @app.post('/user')
+        def post():
+            data = request.get_json()
+            if not data:
+                return {"errors": "User required"}, 400
+            elif data.get('name', None):
+                users[data['name']] = data
+                return {"data": f"User {data['name']} is created!"}, 201
+            else:
+                return {"errors": {"name": "This field is required"}}, 422
+
+        @app.get('/user/<username>')
+        def get(username):
+            user = users.get(username, None)
+            if user:
+                return {"data": f"My name is {user['name']}"}, 200
+            else:
+                return {"data": "User not found"}, 404
+
+        @app.patch('/user/<username>')
+        def update(username):
+            data = request.get_json()
+            if not data:
+                return {"errors": "User required"}, 400
+            user = users.get(username, None)
+            if user:
+                users[username].update(data)
+                return {"data": f"My name is {data['name']}"}, 200
+            else:
+                return {"data": "User not found"}, 404
+
+        @app.delete('/user/<username>')
+        def delete(username):
+            user = users.get(username, None)
+            if user:
+                users.pop(username)
+                return "", 204
