55a30a37a84df20b2e0e8de61e3713dabad29e82
Sergei Bragin <braginsm@gmail.com>
2023-05-29 12:20:33 +0000

    2.4 refactor the lists' solution to recursion

--- lists/lists.py
+++ lists/lists.py
@@ -1,26 +1,30 @@
+from functools import reduce
+
 class ListExercise:
     @staticmethod
     def replace(input_list: list[int]) -> list[int]:
         if len(input_list) == 0:
             return input_list
 
-        max_value = max(input_list)
+        max_value = reduce(lambda result, element: element if element > result else result, input_list)
 
         return list(map(lambda element: element if element < 0 else max_value, input_list))
 
     @staticmethod
     def search(input_list: list[int], query: int) -> int:
-        start_index = 0
-        end_index = len(input_list)
-        search_list = input_list
-        while len(search_list) > 0:
-            middle_index = (end_index + start_index) // 2
-            if input_list[middle_index] == query:
-                return middle_index
-            if input_list[middle_index] > query:
-                end_index = middle_index
+        def serching(input_sub_list: list[int], start_index: int) -> int:
+            length = len(input_sub_list)
+            if length == 0:
+                return -1
+            
+            middle_index = len(input_sub_list) // 2
+            if input_sub_list[middle_index] == query:
+                return middle_index + start_index
+            
+            if input_sub_list[middle_index] < query:
+                new_start_index = middle_index + 1
+                return serching(input_sub_list[new_start_index:], new_start_index + start_index)
             else:
-                start_index = middle_index + 1
-            search_list = input_list[start_index:end_index]
-
-        return -1
+                return serching(input_sub_list[:middle_index], start_index)
+            
+        return serching(input_list, 0)
