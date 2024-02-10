a4988fb412e5c2bf472e258458b0c1ff8a634fb1
platinumscatter <platinumscatter@gmail.com>
2024-02-10 08:57:23 +0300

    bisearch and replace

--- lists/lists.py
+++ lists/lists.py
@@ -1,23 +1,37 @@
 class ListExercise:
     @staticmethod
     def replace(input_list: list[int]) -> list[int]:
-        """
-        Заменить все положительные элементы целочисленного списка на максимальное значение
-        элементов списка.
+        def find_max(input_list: list[int]) -> int:
+            max_so_far = float("-inf")
+            for num in input_list:
+                if num > max_so_far:
+                    max_so_far = num
+            return max_so_far
 
-        :param input_list: Исходный список
-        :return: Список с замененными элементами
-        """
-        pass
+        def replace_list(input_list: list[int]) -> list[int]:
+            maximum = find_max(input_list)
+            for index, num in enumerate(input_list):
+                if num < maximum and num > 0:
+                    input_list[index] = maximum
+            return input_list
+
+        return replace_list(input_list)
 
     @staticmethod
     def search(input_list: list[int], query: int) -> int:
-        """
-        Реализовать двоичный поиск
-        Функция должна возвращать индекс элемента
+        def recursive(input_list: list[int], query: int, lo=0, hi=None):
+            if hi is None:
+                hi = len(input_list) - 1
+            if lo > hi:
+                return -1
 
-        :param input_list: Исходный список
-        :param query: Искомый элемент
-        :return: Номер элемента
-        """
-        pass
+            mid = (lo + hi) // 2
+            aux = input_list[mid]
+
+            if aux == query:
+                return mid
+            if aux > query:
+                return recursive(input_list, query, lo, mid - 1)
+            return recursive(input_list, query, mid + 1, hi)
+
+        return recursive(input_list, query)
