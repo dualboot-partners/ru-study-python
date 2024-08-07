9926ca1bb81f957fa3159bb2e014a2d5ab8bc44b
SakeXYZ <islamshamshiev2002@gmail.com>
2024-08-07 20:43:23 +0600

    test Flask completed

--- flask_exercises/flask_excercises.py
+++ flask_exercises/flask_excercises.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, request, jsonify
 
 
 class FlaskExercise:
@@ -28,4 +28,43 @@ class FlaskExercise:
 
     @staticmethod
     def configure_routes(app: Flask) -> None:
-        pass
+
+        users = {}
+        @app.route('/user', methods=['POST'])
+        def create_user():
+            data = request.json
+            if 'name' not in data:
+                return jsonify({"errors": {"name": "This field is required"}}), 422
+            username = data['name']
+            users[username] = {"age": data.get('age', None)}
+            return jsonify({"data": f"User {username} is created!"}), 201
+
+        @app.route('/user/<name>', methods=['GET'])
+        def read_user(name):
+            if name not in users:
+                return jsonify({"errors": {"name": "User not found"}}), 404
+            return jsonify({"data": f"My name is {name}"}), 200
+
+        @app.route('/user/<name>', methods=['PATCH'])
+        def update_user(name):
+            if name not in users:
+                return jsonify({"errors": {"name": "User not found"}}), 404
+            data = request.json
+            if 'name' not in data:
+                return jsonify({"errors": {"name": "This field is required"}}), 422
+            new_name = data['name']
+            users[new_name] = users.pop(name)
+            return jsonify({"data": f"My name is {new_name}"}), 200
+
+        @app.route('/user/<name>', methods=['DELETE'])
+        def delete_user(name):
+            if name not in users:
+                return jsonify({"errors": {"name": "User not found"}}), 404
+            del users[name]
+            return '', 204
+
+
+
+
+
+
